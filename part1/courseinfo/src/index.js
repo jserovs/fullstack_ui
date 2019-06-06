35adee0dd30897ecfb7fb3cdc4e6d2c24500ddc2
jserovs <jevgenijs.serovs@gmail.com>
2019-06-06 15:31:07 +0300

    part 1.1 ready

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (params) => {
+  return (
+    <div>
+      <h1>{params.course}</h1>
+    </div>
+  )
+}
+
+const Total = (params) => {
+  return (
+    <div>
+      <p>Number of exercises {params.number}</p>
+    </div>
+  )
+}
+
+const Content = (params) => {
+  return (
+    <div>
+      <p>
+        {params.part} {params.exercise}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+
+      <div>
+        <Header course={course} />
+        <Content part={part1} exercise={exercises1}/>
+        <Content part={part2} exercise={exercises2}/>
+        <Content part={part3} exercise={exercises3}/>        
+        <Total number={exercises1+exercises2+exercises3} />
+      </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
