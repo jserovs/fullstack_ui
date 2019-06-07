8ea5bff24f332fccca6c04734a4ce2205755c1c5
jserovs <jevgenijs.serovs@gmail.com>
2019-06-07 08:43:50 +0300

    part 1.5 ready

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (params) => {
-  console.log (params)
-  return (
-    <div>
-      <h1>{params.course}</h1>
-    </div>
-  )
-}
-
-const Total = (params) => {
-  let sum=0
-  params.parts.forEach(part => {
-    console.log(part)
-    sum+=part.exercises
-  })
-  return (    
-    <div>
-      <p>Number of exercises {sum}</p>     
-    </div>
-  )
-}
-
-const Content = (params) => {
-  console.log(params)
-  return (    
-    <div>
-     {params.parts.map(part => <p>{part.name} {part.exercises}</p>)}
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -49,12 +18,18 @@ const App = () => {
     }
   ]
 
+  let sum=0
+  parts.forEach(part => {
+    console.log(part)
+    sum+=part.exercises
+  })
+
   return (
 
       <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Total parts={parts} />
+        <h1>{course}</h1>
+        {parts.map(part => <p>{part.name} {part.exercises}</p>)}        
+        <p>Number of exercises {sum}</p>  
       </div>
   )
 }
