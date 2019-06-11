249ccd257a1d4c8669bd0ad3bf3c057a4c83185a
jserovs <jevgenijs.serovs@gmail.com>
2019-06-11 07:21:12 +0300

    part 2.8 done

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -4,27 +4,31 @@ import Person from './Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      phone: '0000000'}
   ])
-  const [newName, setNewName] = useState('test')
+  const [newName, setNewName] = useState('')
 
-  const handleNoteChange = (event) => {
+  const [newPhone, setNewPhone] = useState('')
+
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
   }
 
   const addPerson = (event) => {
     console.log(persons)
     event.preventDefault()
     const copy = [...persons]
-    if (persons.find(person => person.name === newName) === undefined) {
-      copy.push({name: newName})
+    if (persons.find(person => person.name === newName) === undefined && persons.find(person => person.phone === newPhone) === undefined) {
+      copy.push({name: newName, phone: newPhone})
       setPersons(copy)
     } else {
       window.alert(`${newName} is already added to phonebook`)
     }
-
-
-
   }
 
 
@@ -33,7 +37,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNoteChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          phone: <input value={newPhone} onChange={handlePhoneChange} />
         </div>
         <div>debug: {newName}</div>
         <div>
