21d42f0fe81aa2ed83ce906f1f23307a7b526f3c
jserovs <jevgenijs.serovs@gmail.com>
2019-06-11 16:13:09 +0300

    part 2.11 ready

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 import Filter from './Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(()=>{
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('got date')
+        setPersons(response.data)
+      })
+  },[])
 
   const [filter, setFilter] = useState("")
 
